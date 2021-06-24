b68343ad1bb96d2cd33009f2afc919a0529864ad
Vadim Taits <taizvadim@gmail.com>
2021-06-24 17:06:30 +0300

    [react-redux-isomorphic-filterlist] Export types

--- packages/react-redux-isomorphic-filterlist/src/index.ts
+++ packages/react-redux-isomorphic-filterlist/src/index.ts
@@ -2,3 +2,10 @@ export { useFilterlistIsomorphic } from './useFilterlistIsomorphic';
 export {
   LoadListError,
 } from './errors';
+
+export type {
+  ItemsLoader,
+  Params,
+  IsomorphicErrorType,
+  UseFilterlistIsomorphicReturn,
+} from './types';
