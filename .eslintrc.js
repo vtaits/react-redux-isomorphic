6d7b2b61e23acbc6b91ed0eece10b413669ffb12
Vadim Taits <taizvadim@gmail.com>
2021-08-30 17:11:09 +0300

    Upgrade eslint packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'airbnb',
     'airbnb-typescript',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
